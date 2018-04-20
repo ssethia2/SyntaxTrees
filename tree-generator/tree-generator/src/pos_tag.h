9a92af557668989292a368c4bca4057bc7458af2
Satvik Sethia <satviksethia@gmail.com>
2018-04-20 16:53:49 -0500

    Created project for backend code.

---
+++ tree-generator/tree-generator/src/pos_tag.h
@@ -0,0 +1,8 @@
+#ifndef pos_tag_h
+#define pos_tag_h
+
+#include "stdafx.h"
+
+std::string test_lib();
+
+#endif // !pos_tag_h
---
+++ tree-generator/tree-generator/test/pos_tag_tests.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "../src/pos_tag.h"
+
+TEST_CASE("...") {
+	REQUIRE("a" == test());
+}
