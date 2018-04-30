0238bc735934fcb93915bad795d0d1fec4fa93ad
Satvik Sethia <satviksethia@gmail.com>
2018-04-30 16:21:54 -0500

    Changed background and added font

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -8,6 +8,9 @@ using std::vector;
 //--------------------------------------------------------------
 void ofApp::setup() {
 	syntax_ = "";
+	ofBackground(0);
+	prompt_font_.load("itcblkad.ttf", 28);
+	user_font_.load("itcblkad.ttf", 22);
 }
 
 //--------------------------------------------------------------
@@ -18,21 +21,21 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw() {
 	if (current_state_ == TAKE_INPUT) {
-		ofDrawBitmapString("Enter your input below. Press return to draw tree.", 500, 500);
-		ofDrawBitmapString(syntax_, 500, 520);
+		prompt_font_.drawString("Enter your input below. Press return to draw tree.", ofGetWidth()/2, ofGetHeight()/1.5);
+		user_font_.drawString(syntax_, ofGetWidth() / 2 + 10, ofGetHeight() / 1.5 + 30);
 	}
 
 	if (current_state_ == DRAW_TREE) {
 		/*std::vector<string> tags = {"D", "N", "V", "D", "N", "P", "D", "N"};
 		BinaryTree::BinaryTree syntax_tree = BinaryTree::BinaryTree(tags);*/
-		int x = pow(2.0, 6.0) - 1;
+		int x = pow(2.0, 4.0) - 1;
 		for (int i = 0; i < log2(x); i++) {
 			for (int j = 1; j <= pow(2.0, i); j++) {
-				ofDrawCircle(j * ofGetWidth() / (pow(2.0, i + 1) + 2.0), (i + 2)*ofGetHeight() / x, 200 / x);
+				ofDrawCircle(j * ofGetWidth() / (pow(2.0, i + 1) + 2.0), (i + 2)*ofGetHeight() / x, 250 / x);
 			}
 		}
 
-		ofDrawBitmapString("Press R to reset.", 500, 500);
+		prompt_font_.drawString("Press R to reset.", 500, 500);
 	}
 }
 
@@ -47,7 +50,7 @@ void ofApp::keyPressed(int key) {
 		if (key == OF_KEY_BACKSPACE && syntax_.size() > 0) { // backspace
 			syntax_ = syntax_.substr(0, syntax_.size() - 1); // delete one character
 		}
-		else if (key == OF_KEY_RETURN) {
+		else if (key == OF_KEY_RETURN && syntax_.size() > 0) {
 			current_state_ = DRAW_TREE;
 		}
 		else {
--- src/ofApp.h
+++ src/ofApp.h
@@ -14,8 +14,11 @@ class ofApp : public ofBaseApp {
 
 public:
 	ProjectState current_state_ = TAKE_INPUT;
+
 	string syntax_;
-	ofTrueTypeFont font_;
+	
+	ofTrueTypeFont prompt_font_;
+	ofTrueTypeFont user_font_;
 
 	void setup();
 	void update();
